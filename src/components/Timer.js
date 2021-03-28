504fe6485f0b40df929424d5532540323d446931
browe83 <rowe.brandon.j@gmail.com>
2021-03-28 13:30:12 -0500

    Adds Timer

---
+++ src/components/Timer.js
@@ -0,0 +1,9 @@
+import { Container, Typography } from '@material-ui/core';
+
+export default function Timer() {
+  return (
+    <Container>
+      <Typography>Timer</Typography>
+    </Container>
+  );
+}
